c327fa2795efe596b6f642f3dadeece5a0066fde
Arif Ali <mail@arif-ali.co.uk>
2011-05-20 23:35:47 +0100

    added RM js file

---
+++ RM.js
@@ -0,0 +1,267 @@
+{
+ "version": 1,
+ "homepage": "http://www.cyanogenmod.com/",
+ "donate": "https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=3283920",
+ "roms": [
+ {
+  "modversion": "CyanogenMod-7-05202011-NIGHTLY-Leo",
+  "incremental": "20110520",
+  "name": "CM7 - Build #20110520",
+  "urls": ["http://cmleonightly1.co.cc/rom/cm_leo_full-20110520.zip"],
+  "device": "leo",
+  "addons": [{
+    "name": "Google Apps",
+    "urls": ["http://goo-inside.me/gapps/gapps-gb-20110307-signed.zip"]
+   },
+   {
+    "name": "cLK patch",
+    "urls": ["http://cmleonightly1.co.cc/cLK/charan_05152011_cLK_ppp_cache.zip"]
+   },
+   {
+    "name": "3rd Party",
+    "urls": ["http://cmleonightly1.co.cc/misc/3rdParty-20110517.zip"]
+   }
+   ],
+   "choices": [{
+     "name": "Kernel",
+     "options": [{
+       "name": "Main (charans)",
+       "url": ""
+     },
+     {
+       "name": "tytung r10",
+       "url": "http://cmleonightly1.co.cc/kernels/tytung_r10_charan_clk.zip"
+     },
+     {
+       "name": "rafpigna 1r9",
+       "url": "http://cmleonightly1.co.cc/kernels/rafpigna_1r9_charan_clk.zip"
+     }]
+    }],
+   "product": "CyanogenModLeoNightly",
+   "summary": "05-20-2011"
+  },
+ {
+  "modversion": "CyanogenMod-7-05192011-NIGHTLY-Leo",
+  "incremental": "20110519",
+  "name": "CM7 - Build #20110519",
+  "urls": ["http://cmleonightly1.co.cc/rom/cm_leo_full-20110519.zip"],
+  "device": "leo",
+  "addons": [{
+    "name": "Google Apps",
+    "urls": ["http://goo-inside.me/gapps/gapps-gb-20110307-signed.zip"]
+   },
+   {
+    "name": "cLK patch",
+    "urls": ["http://cmleonightly1.co.cc/cLK/charan_05152011_cLK_ppp_cache.zip"]
+   },
+   {
+    "name": "3rd Party",
+    "urls": ["http://cmleonightly1.co.cc/misc/3rdParty-20110517.zip"]
+   }
+   ],
+   "choices": [{
+     "name": "Kernel",
+     "options": [{
+       "name": "Main (charans)",
+       "url": ""
+     },
+     {
+       "name": "tytung r10",
+       "url": "http://cmleonightly1.co.cc/kernels/tytung_r10_charan_clk.zip"
+     },
+     {
+       "name": "rafpigna 1r9",
+       "url": "http://cmleonightly1.co.cc/kernels/rafpigna_1r9_charan_clk.zip"
+     }]
+    }],
+   "product": "CyanogenModLeoNightly",
+   "summary": "05-19-2011"
+  },
+ {
+  "modversion": "CyanogenMod-7-05182011-NIGHTLY-Leo",
+  "incremental": "20110518",
+  "name": "CM7 - Build #20110518",
+  "urls": ["http://cmleonightly1.co.cc/rom/cm_leo_full-20110518.zip"],
+  "device": "leo",
+  "addons": [{
+    "name": "Google Apps",
+    "urls": ["http://goo-inside.me/gapps/gapps-gb-20110307-signed.zip"]
+   },
+   {
+    "name": "cLK patch",
+    "urls": ["http://cmleonightly1.co.cc/cLK/charan_05152011_cLK_ppp_cache.zip"]
+   },
+   {
+    "name": "3rd Party",
+    "urls": ["http://cmleonightly1.co.cc/misc/3rdParty-20110517.zip"]
+   }
+   ],
+   "choices": [{
+     "name": "Kernel",
+     "options": [{
+       "name": "Main (charans)",
+       "url": ""
+     },
+     {
+       "name": "tytung r10",
+       "url": "http://cmleonightly1.co.cc/kernels/tytung_r10_charan_clk.zip"
+     }]
+    }],
+   "product": "CyanogenModLeoNightly",
+   "summary": "05-18-2011"
+  },
+ {
+  "modversion": "CyanogenMod-7-05172011-NIGHTLY-Leo",
+  "incremental": "20110517",
+  "name": "CM7 - Build #20110517",
+  "urls": ["http://cmleonightly1.co.cc/rom/cm_leo_full-20110517.zip"],
+  "device": "leo",
+  "addons": [{
+    "name": "Google Apps",
+    "urls": ["http://goo-inside.me/gapps/gapps-gb-20110307-signed.zip"]
+   },
+   {
+    "name": "cLK patch",
+    "urls": ["http://cmleonightly1.co.cc/cLK/charan_05152011_cLK_ppp_cache.zip"]
+   },
+   {
+    "name": "3rd Party",
+    "urls": ["http://cmleonightly1.co.cc/misc/3rdParty-20110517.zip"]
+   }
+   ],
+   "choices": [{
+     "name": "Kernel",
+     "options": [{
+       "name": "Main (charans)",
+       "url": ""
+     },
+     {
+       "name": "tytung r10",
+       "url": "http://cmleonightly1.co.cc/kernels/tytung_r10_charan_clk.zip"
+     }]
+    }],
+   "product": "CyanogenModLeoNightly",
+   "summary": "05-17-2011"
+  },
+ {
+  "modversion": "CyanogenMod-7-05162011-NIGHTLY-Leo",
+  "incremental": "20110516",
+  "name": "CM7 - Build #20110516",
+  "urls": ["http://cmleonightly1.co.cc/rom/cm_leo_full-20110516.zip"],
+  "device": "leo",
+  "addons": [{
+    "name": "Google Apps",
+    "urls": ["http://goo-inside.me/gapps/gapps-gb-20110307-signed.zip"]
+   },
+   {
+    "name": "cLK patch",
+    "urls": ["http://cmleonightly1.co.cc/cLK/charan_05152011_cLK_ppp_cache.zip"]
+   },
+   {
+    "name": "3rd Party",
+    "urls": ["http://cmleonightly1.co.cc/misc/3rdParty-20110517.zip"]
+   }
+   ],
+   "product": "CyanogenModLeoNightly",
+   "summary": "05-16-2011"
+  },
+ {
+  "modversion": "CyanogenMod-7-05152011-NIGHTLY-Leo",
+  "incremental": "20110515",
+  "name": "CM7 - Build #20110515",
+  "urls": ["http://cmleonightly1.co.cc/rom/cm_leo_full-20110515.zip"],
+  "device": "leo",
+  "addons": [{
+    "name": "Google Apps",
+    "urls": ["http://goo-inside.me/gapps/gapps-gb-20110307-signed.zip"]
+   },
+   {
+    "name": "cLK patch",
+    "urls": ["http://cmleonightly1.co.cc/cLK/charan_05152011_cLK_ppp_cache.zip"]
+   },
+   {
+    "name": "3rd Party",
+    "urls": ["http://cmleonightly1.co.cc/misc/3rdParty-20110517.zip"]
+   }
+   ],
+   "choices": [{
+     "name": "Kernel",
+     "options": [{
+       "name": "Main (charans)",
+       "url": ""
+     },
+     {
+       "name": "tytung r10",
+       "url": "http://cmleonightly1.co.cc/kernels/tytung_r10_charan_clk.zip"
+     }]
+    }],
+   "product": "CyanogenModLeoNightly",
+   "summary": "05-15-2011"
+  },
+ {
+  "modversion": "CyanogenMod-7-05142011-NIGHTLY-Leo",
+  "incremental": "20110514",
+  "name": "CM7 - Build #20110514",
+  "urls": ["http://cmleonightly1.co.cc/rom/cm_leo_full-20110514.zip"],
+  "device": "leo",
+  "addons": [{
+    "name": "Google Apps",
+    "urls": ["http://goo-inside.me/gapps/gapps-gb-20110307-signed.zip"]
+   },
+   {
+    "name": "cLK patch",
+    "urls": ["http://cmleonightly1.co.cc/cLK/charan_05142011_cLK_ppp_cache.zip"]
+   },
+   {
+    "name": "3rd Party",
+    "urls": ["http://cmleonightly1.co.cc/misc/3rdParty-20110517.zip"]
+   }
+   ],
+   "choices": [{
+     "name": "Kernel",
+     "options": [{
+       "name": "Main (charans)",
+       "url": ""
+     },
+     {
+       "name": "tytung r10",
+       "url": "http://cmleonightly1.co.cc/kernels/tytung_r10_charan_clk.zip"
+     }]
+    }],
+   "product": "CyanogenModLeoNightly",
+   "summary": "05-14-2011"
+  },
+ {
+  "modversion": "CyanogenMod-7-05132011-NIGHTLY-Leo",
+  "incremental": "20110513",
+  "name": "CM7 - Build #20110513",
+  "urls": ["http://cmleonightly1.co.cc/rom/cm_leo_full-20110513.zip"],
+  "device": "leo",
+  "addons": [{
+    "name": "Google Apps",
+    "urls": ["http://goo-inside.me/gapps/gapps-gb-20110307-signed.zip"]
+   },
+   {
+    "name": "cLK patch",
+    "urls": ["http://cmleonightly1.co.cc/cLK/charan_05112011_cLK_ppp_cache.zip"]
+   },
+   {
+    "name": "3rd Party",
+    "urls": ["http://cmleonightly1.co.cc/misc/3rdParty-20110517.zip"]
+   }
+   ],
+   "choices": [{
+     "name": "Kernel",
+     "options": [{
+       "name": "Main (charans)",
+       "url": ""
+     },
+     {
+       "name": "tytung r10",
+       "url": "http://cmleonightly1.co.cc/kernels/tytung_r10_charan_clk.zip"
+     }]
+    }],
+   "product": "CyanogenModLeoNightly",
+   "summary": "05-13-2011"
+  }
+]}
